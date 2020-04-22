7dfc3cad1901a730dec735755ecebf9c940b7035
lucask92 <61902274+lucask92@users.noreply.github.com>
2020-04-23 00:04:09 +0900

    잡코리아 링크

--- treeon_jobkorea.py
+++ treeon_jobkorea.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from pymongo import MongoClient           # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
-client = MongoClient('mongodb://test:test@15.164.166.116', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+client = MongoClient('mongodb://skylark:skylark@54.180.94.231', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
 db = client.projectTreeon                      # 'dbsparta'라는 이름의 db를 만듭니다.
 
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
@@ -20,6 +20,8 @@ jobkorea_enddate = soup.select("#dev-gi-list > div > div.tplList.tplJobList > ta
 jobkorea_link = soup.select("#dev-gi-list > div > div.tplList.tplJobList > table > tbody > tr > td.tplTit > div > strong > a")
 
 # 크롤링 내용
+result = []
+
 for item in zip(jobkorea_startdate, jobkorea_title, jobkorea_position, jobkorea_name, jobkorea_region, jobkorea_enddate, jobkorea_link):
     if db.information.find_one({"title" : item[1].text.strip()}, {'_id': 0}) == None:
         db.information.insert_one({
@@ -30,4 +32,4 @@ for item in zip(jobkorea_startdate, jobkorea_title, jobkorea_position, jobkorea_
             "region": item[4].text.strip(), 
             "enddate": item[5].text.strip(),
             "link": 'www.jobkorea.co.kr'+ item[6].attrs['href']
-            })
+        })
